1d98a9c15b7f41d1295eaf50ba8083ee3a317cfc
Justfer Himbing <justfer@riverph.com>
2025-11-23 03:03:05 +0000

    File changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { RebuildEnvironmentDialog } from "@/components/rebuild-environment-dialog";
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Hello, World!!</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-muted/40">
+      <RebuildEnvironmentDialog />
     </main>
   );
 }
